c6ca0b79e85a44eff2c1dea6c94bc60f62dcb1b6
Juan Gabriel <juangabriel@frogames.es>
2020-02-06 16:01:55 +0100

    Proyecto de prueba

---
+++ prueba/prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Juan Gabriel Gomila Salas on 06/02/2020.
+//  Copyright © 2020 Frogames. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
